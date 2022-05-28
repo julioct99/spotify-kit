7b15f6d7b86844946864bf09036ff5dcaa90644b
julioct99 <jcasallot01@gmail.com>
2022-05-28 18:59:15 +0200

    Divide components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import TopTracks from './TopTracks'
 
 const BASE_SPOTIFY_ACCOUNTS_URI = 'https://accounts.spotify.com'
-const BASE_SPOTIFY_API_URI = 'https://api.spotify.com/v1'
 const AUTHORIZE_ENDPOINT = `${BASE_SPOTIFY_ACCOUNTS_URI}/authorize`
-const MY_TOP_TRACKS_ENDPOINT = `${BASE_SPOTIFY_API_URI}/me/top/tracks/`
 
 const CLIENT_ID = '5e6cead388534d1d86fc41a02419a791'
 const REDIRECT_URI = 'http://localhost:3000/'
@@ -12,7 +10,16 @@ const SCOPES = [
   'user-read-currently-playing',
   'user-read-playback-state',
   'user-top-read',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'playlist-modify-public',
 ].join(' ')
+
+export interface AuthObject {
+  access_token: string
+  token_type: string
+  expires_in: string
+}
 
 const getSpotifyAuthParams = () => {
   const hashContent = window.location.hash.substring(1)
@@ -38,17 +45,12 @@ const login = () => {
 }
 
 function App() {
-  const [token, setToken] = useState<string>()
-  const [tokenType, setTokenType] = useState<string>()
-  const [topTracks, setTopTracks] = useState<SpotifyApi.TrackObjectFull[]>()
-  const [nextUrl, setNextUrl] = useState<string>()
-  const [previousUrl, setPreviousUrl] = useState<string>()
+  const [auth, setAuth] = useState<AuthObject>()
 
   useEffect(() => {
     const params = getSpotifyAuthParams()
     const { access_token, token_type, expires_in } = params
-    setToken(access_token)
-    setTokenType(token_type)
+    setAuth(params)
 
     localStorage.clear()
     localStorage.setItem('accessToken', access_token)
@@ -56,79 +58,15 @@ function App() {
     localStorage.setItem('expiresIn', expires_in)
   }, [])
 
-  const loadTopTracks = async () => {
-    const url = MY_TOP_TRACKS_ENDPOINT
-    const response = await axios.get<SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>>(
-      url,
-      {
-        headers: {
-          Authorization: `${tokenType} ${token}`,
-        },
-      }
-    )
-    console.log('response.data :>> ', response.data)
-    setTopTracks(response.data.items)
-    if (response.data.next) setNextUrl(response.data.next)
-    if (response.data.previous) setPreviousUrl(response.data.previous)
-  }
-
-  const loadNextTopTracksPage = async () => {
-    if (!nextUrl) return
-
-    const url = nextUrl || ''
-    const response = await axios.get<SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>>(
-      url,
-      {
-        headers: {
-          Authorization: `${tokenType} ${token}`,
-        },
-      }
-    )
-    setTopTracks(response.data.items)
-    if (response.data.next) setNextUrl(response.data.next)
-    if (response.data.previous) setPreviousUrl(response.data.previous)
-  }
-
-  const loadPreviousTopTracksPage = async () => {
-    if (!previousUrl) return
-
-    const url = previousUrl || ''
-    const response = await axios.get<SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>>(
-      url,
-      {
-        headers: {
-          Authorization: `${tokenType} ${token}`,
-        },
-      }
-    )
-    setTopTracks(response.data.items)
-    if (response.data.next) setNextUrl(response.data.next)
-    if (response.data.previous) setPreviousUrl(response.data.previous)
-  }
+  const isLoggedIn = !!auth && !!auth.access_token
 
   return (
     <>
-      <h1>Spotify Login</h1>
-      <button onClick={login}>Login to Spotify</button>
-      <button onClick={loadTopTracks}>Load Top Tracks</button>
-      <button onClick={loadPreviousTopTracksPage}>Previous page</button>
-      <button onClick={loadNextTopTracksPage}>Next page</button>
-      <h1>Top tracks</h1>
-      <ul>
-        {topTracks?.map((track) => (
-          <li key={track.id}>
-            <p>
-              {track.name} <strong>by</strong> {track.artists[0].name}
-            </p>
-            <p>{track.preview_url}</p>
-            <img width={150} src={track.album.images[0].url} alt='' />
-            <audio controls>
-              <source src={track.preview_url || ''} type='audio/mp3' />
-            </audio>
-            <br />
-          </li>
-        ))}
-      </ul>
+      {isLoggedIn ? (
+        <TopTracks auth={auth} />
+      ) : (
+        <button onClick={login}>Login to Spotify</button>
+      )}
     </>
   )
 }
---
+++ src/TopTracks.tsx
@@ -0,0 +1,95 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { AuthObject } from './App'
+
+interface TopTracksProps {
+  auth: AuthObject
+}
+
+const BASE_SPOTIFY_API_URI = 'https://api.spotify.com/v1'
+const MY_TOP_TRACKS_ENDPOINT = `${BASE_SPOTIFY_API_URI}/me/top/tracks/?time_range=long_term`
+
+const TopTracks: React.FunctionComponent<TopTracksProps> = ({ auth }) => {
+  const [topTracks, setTopTracks] = useState<SpotifyApi.TrackObjectFull[]>()
+  const [nextUrl, setNextUrl] = useState<string>()
+  const [previousUrl, setPreviousUrl] = useState<string>()
+
+  const token = auth.access_token
+  const tokenType = auth.token_type
+
+  const loadTopTracks = async () => {
+    const url = MY_TOP_TRACKS_ENDPOINT
+    const response = await axios.get<SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>>(
+      url,
+      {
+        headers: {
+          Authorization: `${tokenType} ${token}`,
+        },
+      }
+    )
+    console.log('response.data :>> ', response.data)
+    setTopTracks(response.data.items)
+    if (response.data.next) setNextUrl(response.data.next)
+    if (response.data.previous) setPreviousUrl(response.data.previous)
+  }
+
+  const loadNextTopTracksPage = async () => {
+    if (!nextUrl) return
+
+    const url = nextUrl || ''
+    const response = await axios.get<SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>>(
+      url,
+      {
+        headers: {
+          Authorization: `${tokenType} ${token}`,
+        },
+      }
+    )
+    setTopTracks(response.data.items)
+    if (response.data.next) setNextUrl(response.data.next)
+    if (response.data.previous) setPreviousUrl(response.data.previous)
+  }
+
+  const loadPreviousTopTracksPage = async () => {
+    if (!previousUrl) return
+
+    const url = previousUrl || ''
+    const response = await axios.get<SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>>(
+      url,
+      {
+        headers: {
+          Authorization: `${tokenType} ${token}`,
+        },
+      }
+    )
+    setTopTracks(response.data.items)
+    if (response.data.next) setNextUrl(response.data.next)
+    if (response.data.previous) setPreviousUrl(response.data.previous)
+  }
+
+  return (
+    <>
+      <h1>Spotify Login</h1>
+      <button onClick={loadTopTracks}>Load Top Tracks</button>
+      <button onClick={loadPreviousTopTracksPage}>Previous page</button>
+      <button onClick={loadNextTopTracksPage}>Next page</button>
+      <h1>Top tracks</h1>
+      <ul>
+        {topTracks?.map((track) => (
+          <li key={track.id}>
+            <p>
+              {track.name} <strong>by</strong> {track.artists[0].name}
+            </p>
+            <img width={150} src={track.album.images[0].url} alt='' />
+            <audio controls>
+              <source src={track.preview_url || ''} type='audio/mp3' />
+            </audio>
+            <br />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default TopTracks
