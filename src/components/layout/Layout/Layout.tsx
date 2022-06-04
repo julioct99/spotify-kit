59c976185c1d25a3944cfb11941bdd565b027dc3
julioct99 <jcasallot01@gmail.com>
2022-06-04 21:18:08 +0200

    Make sidebar temporary on mobile

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,12 +1,22 @@
+import { useMediaQuery, useTheme } from '@mui/material'
 import { FunctionComponent } from 'react'
+
 import MiniDrawer from '../Drawer/Drawer'
+import MobileLayout from './MobileLayout'
 
 interface LayoutProps {
   children: JSX.Element
 }
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
-  return <MiniDrawer pageContent={children} />
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'))
+
+  return isMobile ? (
+    <MobileLayout pageContent={children} />
+  ) : (
+    <MiniDrawer pageContent={children} />
+  )
 }
 
 export default Layout
---
+++ src/components/layout/Layout/MobileLayout.tsx
@@ -0,0 +1,77 @@
+import {
+  AppBar,
+  Box,
+  Container,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useState } from 'react'
+
+import DrawerItems from '../Drawer/DrawerItems'
+import { MAIN_MENU_ITEMS } from '../Drawer/menuItems'
+import { LayoutSettings } from '../../../shared/settings'
+
+interface MobileLayoutProps {
+  pageContent: JSX.Element
+}
+
+const MobileLayout: React.FunctionComponent<MobileLayoutProps> = ({ pageContent }) => {
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  const handleDrawerToggle = () => {
+    setOpen((prevOpen) => !prevOpen)
+  }
+
+  return (
+    <Box
+      sx={{ display: 'flex' }}
+      style={{ backgroundColor: '#E4E4E4', minHeight: '100vh', height: 'fit-content' }}
+      paddingBottom={6}
+    >
+      <CssBaseline />
+      <AppBar position='fixed'>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap component='div'>
+            SPOTIFY KIT
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant='temporary'
+        open={open}
+        onClose={handleDrawerClose}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{ width: LayoutSettings.DRAWER_WIDTH, display: 'block' }}
+      >
+        <DrawerItems onItemClick={() => {}} drawerOpen={open} items={MAIN_MENU_ITEMS} />
+      </Drawer>
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }} marginTop={10}>
+        <Container>{pageContent}</Container>
+      </Box>
+    </Box>
+  )
+}
+
+export default MobileLayout
